df0154f151da1653285f0fe5152aa1a3bd9201b0
Sanjeev Namjoshi <snamjoshi87@utexas.edu>
2019-11-04 14:53:17 -0600

    Altered pyx imports in utils/anchors.py

--- utils/anchors.py
+++ utils/anchors.py
@@ -14,6 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+import pyximport
+pyximport.install()
+
 import numpy as np
 import keras
 
