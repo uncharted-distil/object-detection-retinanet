34307311434e6b4d8dd9df5b4a23b61ab2c7ce0d
Sanjeev Namjoshi <snamjoshi87@utexas.edu>
2019-11-21 12:02:38 -0600

    Refactoring produce, removed print statements

--- object_detection/object_detection.py
+++ object_detection/object_detection.py
@@ -300,73 +300,32 @@ class ObjectDetectionRNPrimitive(PrimitiveBase[Inputs, Outputs, Params, Hyperpar
         validation_generator = CSVGenerator(self.annotations, self.classes, self.base_dir, self.hyperparams['batch_size'], self.backbone.preprocess_image, shuffle_groups = False)
         return validation_generator
 
+    def _fill_empty_predictions(self, empty_predictions_image_names, d3mIdx_image_mapping):
+        """
+        D3M metrics evaluator needs at least one prediction per image. If RetinaNet does not return 
+        predictions for an image, this method creates a dummy empty prediction row to add to results_df for that
+        missing image. 
 
-    def _get_detections(self, generator, model, iou_threshold, score_threshold, max_detections, save_path):
-        """ 
-        Evaluate a given dataset using a given model.
-
-        Parameters
-        ----------
-        generator       : The generator that represents the dataset to evaluate.
-        model           : The model to evaluate.
-        iou_threshold   : The threshold used to consider when a detection is positive or negative.
-        score_threshold : The score confidence threshold to use for detections.
-        max_detections  : The maximum number of detections to use per image.
-        save_path       : The path to save images with visualized detections to.
-        
-        Returns
-        -------
-        all_detections  : A list containing the predicted boxes for each image in the generator.
+        TODO: DUMMY CONFIDENCE SCORES LOWER AVERAGE PRECISION. FIND A FIX.
         """
 
-        all_detections = [[None for i in range(generator.num_classes()) if generator.has_label(i)] for j in range(generator.size())]
-        
-        for i in range(generator.size()):
-            raw_image    = generator.load_image(i)
-            image        = generator.preprocess_image(raw_image.copy())
-            image, scale = generator.resize_image(image)
+        # Prepare D3M index
+        empty_predictions_d3mIdx = [d3mIdx_image_mapping.get(key) for key in empty_predictions_image_names]
+        empty_predictions_d3mIdx = [item for sublist in empty_predictions_d3mIdx for item in sublist]
 
-            if keras.backend.image_data_format() == 'channels_first':
-                image = image.transpose((2, 0, 1))
+        # Prepare dummy columns
+        d3mIdx = empty_predictions_d3mIdx
+        bounding_box = ["0,0,0,0,0,0,0,0"] * len(empty_predictions_d3mIdx)
+        confidence = [float(0)] * len(empty_predictions_d3mIdx)
 
-            # run network
-            boxes, scores, labels = model.predict_on_batch(np.expand_dims(image, axis=0))[:3]
+        empty_predictions_df = pd.DataFrame({
+            'd3mIndex': d3mIdx,
+            'bounding_box': bounding_box,
+            'confidence': confidence
+        })
 
-            # correct boxes for image scale
-            boxes /= scale
+        return empty_predictions_df
 
-            # select indices which have a score above the threshold
-            indices = np.where(scores[0, :] > score_threshold)[0]
-
-            # select those scores
-            scores = scores[0][indices]
-
-            # find the order with which to sort the scores
-            scores_sort = np.argsort(-scores)[:max_detections]
-
-            # select detections
-            image_boxes      = boxes[0, indices[scores_sort], :]
-            image_scores     = scores[scores_sort]
-            image_labels     = labels[0, indices[scores_sort]]
-            image_detections = np.concatenate([image_boxes, np.expand_dims(image_scores, axis=1), np.expand_dims(image_labels, axis=1)], axis=1)
-
-            ### !!! SAVEPATH CURRENTLY NOT IMPLEMENTED !!!
-            ### This optional feature can be added later, maybe for TA3, allowing images to be output with 
-            ### bounding boxes to a specified directory after evaluation.
-            # if save_path is True:
-            #     draw_annotations(raw_image, generator.load_annotations(i), label_to_name = generator.label_to_name)
-            #     draw_detections(raw_image, image_boxes, image_scores, image_labels, label_to_name = generator.label_to_name, score_threshold = score_threshold)
-
-            #     cv2.imwrite(os.path.join(save_path, '{}.png'.format(i)), raw_image)
-            
-            for label in range(generator.num_classes()):
-                if not generator.has_label(label):
-                    continue
-
-                all_detections[i][label] = image_detections[image_detections[:, -1] == label, :-1]
-
-        return all_detections
-    
     def fit(self, *, timeout: float = None, iterations: int = None) -> CallResult[None]:
         """
         Creates the image generators and then trains RetinaNet model on the image paths in the input 
@@ -434,52 +393,6 @@ class ObjectDetectionRNPrimitive(PrimitiveBase[Inputs, Outputs, Params, Hyperpar
         print(f'Training complete. Training took {time.time()-start_time} seconds.', file = sys.__stdout__)
         return CallResult(None) 
 
-    def _get_annotations(self, generator):
-        """ Get the ground truth annotations from the generator.
-
-        The result is a list of lists such that the size is:
-            all_detections[num_images][num_classes] = annotations[num_detections, 5]
-
-        # Arguments
-            generator : The generator used to retrieve ground truth annotations.
-        # Returns
-            A list of lists containing the annotations for each image in the generator.
-        """
-        all_annotations = [[None for i in range(generator.num_classes())] for j in range(generator.size())]
-
-        for i in range(generator.size()):
-            # load the annotations
-            annotations = generator.load_annotations(i)
-
-            # copy detections to all_annotations
-            for label in range(generator.num_classes()):
-                if not generator.has_label(label):
-                    continue
-
-                all_annotations[i][label] = annotations['bboxes'][annotations['labels'] == label, :].copy()
-
-        return all_annotations
-    
-    def _fill_empty_predictions(self, empty_predictions_image_names):
-        # Prepare D3M index
-        empty_predictions_d3mIdx = [d3mIdx_image_mapping.get(key) for key in empty_predictions_image_names]
-        empty_predictions_d3mIdx = [item for sublist in empty_predictions_d3mIdx for item in sublist]
-
-        # Prepare dummy columns
-        d3mIdx = empty_predictions_d3mIdx
-        #image = empty_predictions_image_names
-        bounding_box = ["0,0,0,0,0,0,0,0"] * len(empty_predictions_d3mIdx)
-        confidence = [float(0)] * len(empty_predictions_d3mIdx)
-
-        empty_predictions_df = pd.DataFrame({
-            'd3mIndex': d3mIdx,
-            #'image': empty_predictions_image_names,
-            'bounding_box': bounding_box,
-            'confidence': confidence
-        })
-
-        return empty_predictions_df
-
     def produce(self, *, inputs: Inputs, timeout: float = None, iterations: int = None) -> CallResult[Outputs]:
         """
         Produce image detection predictions.
@@ -504,65 +417,6 @@ class ObjectDetectionRNPrimitive(PrimitiveBase[Inputs, Outputs, Params, Hyperpar
         # Convert training model to inference model
         inference_model = models.convert_model(self.training_model)
 
-        # Assemble output lists
-        ## Generate predicted bounding boxes (8-coordinate format, list)
-        #all_detections = self._get_detections(generator, inference_model, iou_threshold, score_threshold, max_detections, self.hyperparams['output'])
-        #all_annotations = self._get_annotations(generator)
-
-        # for label in range(generator.num_classes()):
-        #     if not generator.has_label(label):
-        #         continue
-
-        #     false_positives = np.zeros((0,))
-        #     true_positives  = np.zeros((0,))
-        #     scores          = np.zeros((0,))
-        #     num_annotations = 0.0
-        #     all_annotations_list = []
-        #     scores_list = []
-
-        #     for i in range(generator.size()):
-        #         detections           = all_detections[i][label]
-        #         annotations          = all_annotations[i][label]
-        #         num_annotations     += annotations.shape[0]
-        #         detected_annotations = []
-
-        #         all_annotations_list.append(annotations)
-
-        #         for d in detections:
-        #             scores = np.append(scores, d[4])
-
-        #             if annotations.shape[0] == 0:
-        #                 false_positives = np.append(false_positives, 1)
-        #                 true_positives  = np.append(true_positives, 0)
-        #                 continue
-
-        #             overlaps            = compute_overlap(np.expand_dims(d, axis=0), annotations)
-        #             assigned_annotation = np.argmax(overlaps, axis=1)
-        #             max_overlap         = overlaps[0, assigned_annotation]
-
-        #             if max_overlap >= iou_threshold and assigned_annotation not in detected_annotations:
-        #                 false_positives = np.append(false_positives, 0)
-        #                 true_positives  = np.append(true_positives, 1)
-        #                 detected_annotations.append(assigned_annotation)
-        #             else:
-        #                 false_positives = np.append(false_positives, 1)
-        #                 true_positives  = np.append(true_positives, 0)
-
-        # all_annotations_list = np.array(all_annotations_list).tolist()
-
-        # print(false_positives, file = sys.__stdout__)
-        # print(true_positives, file = sys.__stdout__)
-        # print(detected_annotations, file = sys.__stdout__)
-        # print(num_annotations, file = sys.__stdout__)
-        # print(assigned_annotation, file = sys.__stdout__)
-        # print(detections, file = sys.__stdout__)
-        # print(annotations, file = sys.__stdout__)
-        # print(scores, file = sys.__stdout__)
-        # print(overlaps, file = sys.__stdout__)
-        # print(max_overlap, file = sys.__stdout__)
-        # print(len(all_annotations_list), file = sys.__stdout__)
-        # print(len(scores), file = sys.__stdout__)
-
         start_time = time.time()
         print('Starting testing...', file = sys.__stdout__)
 
@@ -572,17 +426,13 @@ class ObjectDetectionRNPrimitive(PrimitiveBase[Inputs, Outputs, Params, Hyperpar
         self.image_paths = np.array([[os.path.join(self.base_dir, filename) for filename in inputs.iloc[:,col]] for self.base_dir, col in zip(self.base_dir, image_cols)]).flatten()
         self.image_paths = pd.Series(self.image_paths)
 
-        # Initialize objects
+        # Initialize output objects
         box_list = []
         score_list = []
         image_name_list = []
 
+        # Predict bounding boxes and confidence socres for each image
         image_list = [x for i, x in enumerate(self.image_paths.tolist()) if self.image_paths.tolist().index(x) == i]
-        
-        print(len(self.image_paths.tolist()), file = sys.__stdout__)
-        print(len(self.annotations['img_file'].tolist()), file = sys.__stdout__)
-        print(len(image_list), file = sys.__stdout__)
-        print(image_list, file = sys.__stdout__)
 
         for i in image_list:
             image = read_image_bgr(i)
@@ -605,38 +455,26 @@ class ObjectDetectionRNPrimitive(PrimitiveBase[Inputs, Outputs, Params, Hyperpar
                 score_list.append(score)
                 image_name_list.append(i * len(b))
 
-        ## Convert predicted boxes from a list of arrays to a list of strings
         print(f'Testing complete. Testing took {time.time()-start_time} seconds.', file = sys.__stdout__)
         
+        ## Convert predicted boxes from a list of arrays to a list of strings
         boxes = np.array(box_list).tolist()
         boxes = list(map(lambda x : [x[0], x[1], x[0], x[3], x[2], x[3], x[2], x[1]], boxes))  # Convert to 8 coordinate format for D3Mmm                
         boxes = list(map(lambda x : ",".join(map(str, x)), boxes))
-        
-        image_name_list = [os.path.basename(list) for list in image_name_list]
 
-        ## Create mapping between image names and D3M index
+        # Create mapping between image names and D3M index
         input_df = pd.DataFrame({
             'd3mIndex': inputs.d3mIndex,
             'image': [os.path.basename(list) for list in self.image_paths]
         })
 
-        print(input_df, file = sys.__stdout__)
-
         d3mIdx_image_mapping = input_df.set_index('image').T.to_dict('list')
 
-        ## Extract values for image name keys and get missing image predictions (if they exist)
+        # Extract values for image name keys and get missing image predictions (if they exist)
+        image_name_list = [os.path.basename(list) for list in image_name_list]
         d3mIdx = [d3mIdx_image_mapping.get(key) for key in image_name_list]
         empty_predictions_image_names = [k for k,v in d3mIdx_image_mapping.items() if v not in d3mIdx]
         d3mIdx = [item for sublist in d3mIdx for item in sublist]   # Flatten list of lists
-
-        ## Generate list of image names and d3m indices corresponding to predicted bounding boxes
-        #img_name = [os.path.basename(list) for list in self.annotations['img_file'].tolist()]
-        #d3m_idx = inputs.d3mIndex.tolist()
-        
-        print(len(d3mIdx), file = sys.__stdout__)
-        print(len(image_name_list), file = sys.__stdout__)
-        print(len(boxes), file = sys.__stdout__)
-        print(len(score_list), file = sys.__stdout__)
 
         ## Assemble in a Pandas DataFrame
         results = pd.DataFrame({
@@ -651,13 +489,11 @@ class ObjectDetectionRNPrimitive(PrimitiveBase[Inputs, Outputs, Params, Hyperpar
         if len(empty_predictions_image_names) != 0:
             # Create data frame of empty predictions for missing each image and concat with results.
             # Sort results_df.
-            empty_predictions_df = _fill_empty_predictions(empty_predictions_image_names)
+            empty_predictions_df = self._fill_empty_predictions(empty_predictions_image_names, d3mIdx_image_mapping)
             results_df = pd.concat([results, empty_predictions_df]).sort_values('d3mIndex')
 
         # Convert to DataFrame container
-        results_df = d3m_DataFrame(results)
-
-        print(results_df, file = sys.__stdout__)
+        results_df = d3m_DataFrame(results_df)
         
         ## Assemble first output column ('d3mIndex)
         col_dict = dict(results_df.metadata.query((metadata_base.ALL_ELEMENTS, 0)))
